5f4c002f9905a1dc0fc6271d21904c34fae6cdd6
Ugoeze-hub <eluchieugoeze@gmail.com>
2025-11-02 03:37:19 +0100

    fixed port issue, should work on leapcell now

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ async def fact_checker_route(request: JsonRpcRequest):
             }
         )
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.getenv("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     import uvicorn
+#     port = int(os.getenv("PORT", 8080))
+#     uvicorn.run(app, host="0.0.0.0", port=port)
